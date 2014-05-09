5ef3a2c2437948a4259b7f3a3bee499486a05206
shibby170 <shibby170@live.com>
2014-05-09 13:58:37 -0400

    text
    Text positiong issue.  App works fine otherwise.

--- js/app.js
+++ js/app.js
@@ -21,6 +21,19 @@ $(document).ready(function() {
     	$('.ryu-throwing').hide();
     	$('.ryu-ready').show();
     });
+    $(document).keydown(function (event) {
+      if (event.which == 88) {
+        $(".ryu-still").hide();
+        $(".ryu-ready").hide();
+        $(".ryu-cool").show();
+      }
+    })
+    .keyup(function (event) {
+      if (event.which == 88) {
+        $(".ryu-cool").hide();
+        $(".ryu-still").show();
+      }
+    });
 });
 
 function playHadouken () {
